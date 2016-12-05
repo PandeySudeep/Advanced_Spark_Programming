99032890b4bf41c1837977713c7b5c945ddd356f
Sudeep Pandey <pandey.datatech@gmail.com>
2016-12-04 20:44:45 -0500

    Adding Source Files

---
+++ src/test/java/advanced/spark/programming/adv_spark_app/CallLog.java
@@ -0,0 +1,12 @@
+package advanced.spark.programming.adv_spark_app;
+
+import java.io.Serializable;
+
+	public class CallLog implements Serializable {
+	  public String callsign;
+	  public Double contactlat;
+	  public Double contactlong;
+	  public Double mylat;
+	  public Double mylong;
+	}
+
